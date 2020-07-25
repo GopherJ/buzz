e326eb7c5e460c7811fda91ed0f34c1af7a0f553
Cheng JIANG <jiang.cheng@vip.163.com>
2020-07-25 22:54:19 +0200

    bump dep version

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,10 @@
 use native_tls::{TlsConnector, TlsStream};
 use rayon::prelude::*;
 
-use std::borrow::Cow;
-use std::collections::BTreeMap;
-use std::fs::File;
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::process::Command;
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
+use std::{
+    borrow::Cow, collections::BTreeMap, fs::File, io::prelude::*,
+    net::TcpStream, process::Command, sync::mpsc, thread, time::Duration,
+};
 
 use directories_next::ProjectDirs;
 
@@ -25,27 +20,30 @@ struct Account {
 }
 
 impl Account {
-    pub fn connect(&self) -> Result<Connection<TlsStream<TcpStream>>, imap::error::Error> {
+    pub fn connect(
+        &self,
+    ) -> Result<Connection<TlsStream<TcpStream>>, imap::error::Error> {
         let tls = TlsConnector::builder().build()?;
-        imap::connect((&*self.server.0, self.server.1), &self.server.0, &tls).and_then(|c| {
-            let mut c = c
-                .login(self.username.trim(), self.password.trim())
-                .map_err(|(e, _)| e)?;
-            let cap = c.capabilities()?;
-            if !cap.has_str("IDLE") {
-                return Err(imap::error::Error::Bad(
-                    cap.iter()
-                        .map(|s| format!("{:?}", s))
-                        .collect::<Vec<_>>()
-                        .join(","),
-                ));
-            }
-            c.select("INBOX")?;
-            Ok(Connection {
-                account: self.clone(),
-                socket: c,
+        imap::connect((&*self.server.0, self.server.1), &self.server.0, &tls)
+            .and_then(|c| {
+                let mut c = c
+                    .login(self.username.trim(), self.password.trim())
+                    .map_err(|(e, _)| e)?;
+                let cap = c.capabilities()?;
+                if !cap.has_str("IDLE") {
+                    return Err(imap::error::Error::Bad(
+                        cap.iter()
+                            .map(|s| format!("{:?}", s))
+                            .collect::<Vec<_>>()
+                            .join(","),
+                    ));
+                }
+                c.select("INBOX")?;
+                Ok(Connection {
+                    account: self.clone(),
+                    socket: c,
+                })
             })
-        })
     }
 }
 
@@ -55,12 +53,19 @@ struct Connection<T: Read + Write> {
 }
 
 impl<T: Read + Write + imap::extensions::idle::SetReadTimeout> Connection<T> {
-    pub fn handle(mut self, account: usize, mut tx: mpsc::Sender<Option<(usize, usize)>>) {
+    pub fn handle(
+        mut self,
+        account: usize,
+        mut tx: mpsc::Sender<Option<(usize, usize)>>,
+    ) {
         loop {
             if let Err(e) = self.check(account, &mut tx) {
                 // the connection has failed for some reason
                 // try to log out (we probably can't)
-                eprintln!("connection to {} failed: {:?}", self.account.name, e);
+                eprintln!(
+                    "connection to {} failed: {:?}",
+                    self.account.name, e
+                );
                 let _ = self.socket.logout();
                 break;
             }
@@ -79,7 +84,10 @@ impl<T: Read + Write + imap::extensions::idle::SetReadTimeout> Connection<T> {
                     return c.handle(account, tx);
                 }
                 Err(e) => {
-                    eprintln!("failed to connect to {}: {:?}", self.account.name, e);
+                    eprintln!(
+                        "failed to connect to {}: {:?}",
+                        self.account.name, e
+                    );
                     thread::sleep(Duration::from_secs(wait));
                 }
             }
@@ -105,61 +113,79 @@ impl<T: Read + Write + imap::extensions::idle::SetReadTimeout> Connection<T> {
                 // there are no messages we haven't already notified about
                 uids.clear();
             }
-            last_notified = std::cmp::max(last_notified, uids.iter().cloned().max().unwrap_or(0));
+            last_notified = std::cmp::max(
+                last_notified,
+                uids.iter().cloned().max().unwrap_or(0),
+            );
 
             let mut subjects = BTreeMap::new();
             if !uids.is_empty() {
-                let uids: Vec<_> = uids.into_iter().map(|v: u32| format!("{}", v)).collect();
-                for msg in self
+                let uids: Vec<_> =
+                    uids.into_iter().map(|v: u32| format!("{}", v)).collect();
+                for msg_header in self
                     .socket
                     .uid_fetch(&uids.join(","), "RFC822.HEADER")?
                     .iter()
+                    .filter_map(|msg| msg.header())
                 {
-                    let msg = msg.header();
-                    if msg.is_none() {
-                        continue;
-                    }
-
-                    match mailparse::parse_headers(msg.unwrap()) {
+                    match mailparse::parse_headers(msg_header) {
                         Ok((headers, _)) => {
                             use mailparse::MailHeaderMap;
 
-                            let subject = match headers.get_first_value("Subject") {
-                                Some(subject) => Cow::from(subject),
-                                None => Cow::from("<no subject>"),
-                            };
+                            let subject =
+                                match headers.get_first_value("Subject") {
+                                    Some(subject) => Cow::from(subject),
+                                    None => Cow::from("<no subject>"),
+                                };
 
                             let date = match headers.get_first_value("Date") {
-                                Some(date) => match chrono::DateTime::parse_from_rfc2822(&date) {
-                                    Ok(date) => date.with_timezone(&chrono::Local),
-                                    Err(e) => {
-                                        println!("failed to parse message date: {:?}", e);
-                                        chrono::Local::now()
+                                Some(date) => {
+                                    match chrono::DateTime::parse_from_rfc2822(
+                                        &date,
+                                    ) {
+                                        Ok(date) => {
+                                            date.with_timezone(&chrono::Local)
+                                        }
+                                        Err(e) => {
+                                            eprintln!("failed to parse message date: {:?}", e);
+                                            chrono::Local::now()
+                                        }
                                     }
-                                },
+                                }
                                 None => chrono::Local::now(),
                             };
 
                             subjects.insert(date, subject);
                         }
-                        Err(e) => println!("failed to parse headers of message: {:?}", e),
+                        Err(e) => eprintln!(
+                            "failed to parse headers of message: {:?}",
+                            e
+                        ),
                     }
                 }
             }
 
             if !subjects.is_empty() {
-                if let Some(notificationcmd) = &self.account.notification_command {
-                    match Command::new("sh").arg("-c").arg(notificationcmd).status() {
+                if let Some(notificationcmd) =
+                    &self.account.notification_command
+                {
+                    match Command::new("sh")
+                        .arg("-c")
+                        .arg(notificationcmd)
+                        .status()
+                    {
                         Ok(s) if s.success() => {}
                         Ok(s) => {
-                            eprint!(
+                            eprintln!(
                                 "Notification command for {} did not exit successfully.",
                                 self.account.name
                             );
                             if let Some(exit_code) = s.code() {
                                 eprintln!(" Exit code: {}", exit_code);
                             } else {
-                                eprintln!(" Process was terminated by a signal.",);
+                                eprintln!(
+                                    " Process was terminated by a signal.",
+                                );
                             }
                         }
                         Err(e) => {
@@ -173,7 +199,7 @@ impl<T: Read + Write + imap::extensions::idle::SetReadTimeout> Connection<T> {
 
                 use notify_rust::{Hint, Notification};
                 let title = format!(
-                    "@{} has new mail ({} unseen)",
+                    "[{}] has new mail ({} unseen)",
                     self.account.name, num_unseen
                 );
 
@@ -186,7 +212,7 @@ impl<T: Read + Write + imap::extensions::idle::SetReadTimeout> Connection<T> {
                 }
                 let body = body.trim_end();
 
-                println!("! {}", title);
+                println!("> {}", title);
                 println!("{}", body);
                 if let Some(mut n) = notification.take() {
                     n.summary(&title).body(&format!(
@@ -200,7 +226,10 @@ impl<T: Read + Write + imap::extensions::idle::SetReadTimeout> Connection<T> {
                             .summary(&title)
                             .body(&format!(
                                 "{}",
-                                askama_escape::escape(body, askama_escape::Html)
+                                askama_escape::escape(
+                                    body,
+                                    askama_escape::Html
+                                )
                             ))
                             .icon("notification-message-email")
                             .hint(Hint::Category("email.arrived".to_owned()))
@@ -239,7 +268,7 @@ fn main() {
         let mut f = match File::open(config) {
             Ok(f) => f,
             Err(e) => {
-                println!("Could not open configuration file buzz.toml: {}", e);
+                eprintln!("Could not open configuration file buzz.toml: {}", e);
                 return;
             }
         };
@@ -263,7 +292,10 @@ fn main() {
             .iter()
             .filter_map(|(name, v)| match v.as_table() {
                 None => {
-                    println!("Configuration for account {} is broken: not a table", name);
+                    println!(
+                        "Configuration for account {} is broken: not a table",
+                        name
+                    );
                     None
                 }
                 Some(t) => {
@@ -272,10 +304,19 @@ fn main() {
                         Some(pwcmd) => pwcmd,
                     };
 
-                    let password = match Command::new("sh").arg("-c").arg(pwcmd).output() {
-                        Ok(output) => String::from_utf8_lossy(&output.stdout).into_owned(),
+                    let password = match Command::new("sh")
+                        .arg("-c")
+                        .arg(pwcmd)
+                        .output()
+                    {
+                        Ok(output) => {
+                            String::from_utf8_lossy(&output.stdout).into_owned()
+                        }
                         Err(e) => {
-                            println!("Failed to launch password command for {}: {}", name, e);
+                            println!(
+                                "Failed to launch password command for {}: {}",
+                                name, e
+                            );
                             return None;
                         }
                     };
@@ -285,7 +326,9 @@ fn main() {
                         server: (
                             match t["server"].as_str() {
                                 Some(v) => v.to_owned(),
-                                None => return parse_failed("server", "string"),
+                                None => {
+                                    return parse_failed("server", "string")
+                                }
                             },
                             match t["port"].as_integer() {
                                 Some(v) => v as u16,
@@ -301,24 +344,31 @@ fn main() {
                             }
                         },
                         password,
-                        notification_command: t.get("notificationcmd").and_then(
-                            |raw_v| match raw_v.as_str() {
+                        notification_command: t
+                            .get("notificationcmd")
+                            .and_then(|raw_v| match raw_v.as_str() {
                                 Some(v) => Some(v.to_string()),
-                                None => return parse_failed("notificationcmd", "string"),
-                            },
-                        ),
+                                None => {
+                                    return parse_failed(
+                                        "notificationcmd",
+                                        "string",
+                                    )
+                                }
+                            }),
                     })
                 }
             })
             .collect(),
         None => {
-            println!("Could not parse configuration file buzz.toml: not a table");
+            eprintln!(
+                "Could not parse configuration file buzz.toml: not a table"
+            );
             return;
         }
     };
 
     if accounts.is_empty() {
-        println!("No accounts in config; exiting...");
+        eprintln!("No accounts in config; exiting...");
         return;
     }
 
@@ -326,13 +376,14 @@ fn main() {
     let mut app = match systray::Application::new() {
         Ok(app) => app,
         Err(e) => {
-            println!("Could not create gtk application: {}", e);
+            eprintln!("Could not create gtk application: {}", e);
             return;
         }
     };
-    if let Err(e) = app.set_icon_from_file("/usr/share/icons/Faenza/stock/24/stock_disconnect.png")
-    {
-        println!("Could not set application icon: {}", e);
+    if let Err(e) = app.set_icon_from_file(
+        "/usr/share/icons/Faenza/stock/24/stock_disconnect.png",
+    ) {
+        eprintln!("Could not set application icon: {}", e);
     }
 
     let (tx, rx) = mpsc::channel();
@@ -356,14 +407,17 @@ fn main() {
                 match account.connect() {
                     Ok(c) => return Some(c),
                     Err(imap::error::Error::Io(e)) => {
-                        println!(
+                        eprintln!(
                             "Failed to connect account {}: {}; retrying in {}s",
                             account.name, e, wait
                         );
                         thread::sleep(Duration::from_secs(wait));
                     }
                     Err(e) => {
-                        println!("{} host produced bad IMAP tunnel: {:?}", account.name, e);
+                        eprintln!(
+                            "{} host produced bad IMAP tunnel: {:?}",
+                            account.name, e
+                        );
                         break;
                     }
                 }
@@ -376,13 +430,15 @@ fn main() {
         .collect();
 
     if accounts.is_empty() {
-        println!("No accounts in config worked; exiting...");
+        eprintln!("No accounts in config worked; exiting...");
         return;
     }
 
     // We have now connected
-    app.set_icon_from_file("/usr/share/icons/Faenza/stock/24/stock_connect.png")
-        .ok();
+    app.set_icon_from_file(
+        "/usr/share/icons/Faenza/stock/24/stock_connect.png",
+    )
+    .ok();
 
     let mut unseen: Vec<_> = accounts.iter().map(|_| 0).collect();
     for (i, conn) in accounts.into_iter().enumerate() {
@@ -400,11 +456,15 @@ fn main() {
         };
         unseen[i] = num_unseen;
         if unseen.iter().sum::<usize>() == 0 {
-            app.set_icon_from_file("/usr/share/icons/oxygen/base/32x32/status/mail-unread.png")
-                .unwrap();
+            app.set_icon_from_file(
+                "/usr/share/icons/oxygen/base/32x32/status/mail-unread.png",
+            )
+            .unwrap();
         } else {
-            app.set_icon_from_file("/usr/share/icons/oxygen/base/32x32/status/mail-unread-new.png")
-                .unwrap();
+            app.set_icon_from_file(
+                "/usr/share/icons/oxygen/base/32x32/status/mail-unread-new.png",
+            )
+            .unwrap();
         }
     }
 }
